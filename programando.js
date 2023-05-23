f1e4cba1fa4e5eefa2a9c708a079177722eed9f8
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-05-22 22:39:52 -0300

    Revisão Aula 2:Seletores (EBAC)

---
+++ programando.js
@@ -0,0 +1,9 @@
+var botao = document.getElementsByTagName('form')[0].querySelector('input[type=submit]')
+
+botao.style.background = 'blue'
+botao.style.color = 'white'
+
+var botao2 = document.getElementsByTagName('form')[1].querySelector('input[type=submit]')
+
+botao2.style.background = 'red'
+botao2.style.color = 'white'
