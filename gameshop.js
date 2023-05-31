fc8aa001129913b27b59c8eeb961ff8c0aa9bfe0
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-05-30 23:25:38 -0300

    Revisão Aula JavaScript (Formulários)

--- gameshop.js
+++ gameshop.js
@@ -6,3 +6,21 @@ function Menu() {
     itens.style.display = 'block'
   }
 }
+
+const form = document.getElementById('fale-conosco')
+
+function validaNome(NomeCompleto) {
+  const NomeComoArray = NomeCompleto.split(' ')
+  return NomeComoArray.length >= 2
+}
+
+form.addEventListener('submit', function(e){
+  e.preventDefault()
+
+  const NomeBeneficiario = document.getElementById('inomeCompleto')
+  if (!validaNome(NomeBeneficiario.value)) {
+    alert('O nome está incompleto!')
+  } else {
+    alert('Está tudo OK!')
+  }
+})
